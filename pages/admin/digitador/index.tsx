7e509372a371af2ac1cde734d25c788d677acff0
angelooz <angelo-mjz7@hotmail.com>
2023-03-22 15:38:07 -0500

    dsfsd

--- pages/admin/digitador/index.tsx
+++ pages/admin/digitador/index.tsx
@@ -30,7 +30,7 @@ export default function PageAdminEventos() {
                     ]}
                 />
                 <Card>
-
+                    sdfsdvsd
                 </Card>
             </Container>
         </>
