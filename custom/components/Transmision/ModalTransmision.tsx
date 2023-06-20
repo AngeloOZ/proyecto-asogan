eca6659882fc18f5f5486f444d1655a8079bc549
desarrolloperseoec <desarrollo@perseo.ec>
2023-06-20 08:43:42 -0500

    redireccionar

--- custom/components/Transmision/ModalTransmision.tsx
+++ custom/components/Transmision/ModalTransmision.tsx
@@ -1,36 +1,45 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Link from 'next/link';
 import { PATH_DASHBOARD_CLEINTE } from 'src/routes/paths';
-import { Modal, Button,Box } from '@mui/material';
+import { Modal, Button, Box } from '@mui/material';
 type Props = {
     open: boolean;
-    rol:string
+    rol: string
 }
-export const ModalTransmision = ({open, rol}:Props) => {
+export const ModalTransmision = ({ open, rol }: Props) => {
+    const buttonRef = useRef<HTMLButtonElement>(null);
+    const timer = setTimeout(() => {
+        if (buttonRef.current)
+            buttonRef.current.click();
+            
+    }, 5000);
+
+
     return (
-        <Modal  aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
-        open={rol == "comprador" ? open: false} >
-            <Box  sx={{
-                        position: "absolute",
-                        top: "50%",
-                        left: "50%",
-                        transform: "translate(-50%, -50%)",
-                        width: 400,
-                        bgcolor: "background.paper",
-                        boxShadow: 24,
-                        p: 4,
-                        borderRadius: 3,
-                    }}>
+        <Modal aria-labelledby="transition-modal-title"
+            aria-describedby="transition-modal-description"
+            open={rol == "comprador" ? open : false} >
+            <Box sx={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                width: 400,
+                bgcolor: "background.paper",
+                boxShadow: 24,
+                p: 4,
+                borderRadius: 3,
+            }}>
                 <h2>La transmisión ha terminado</h2>
-                <Link href={PATH_DASHBOARD_CLEINTE.root}  passHref legacyBehavior>
+                <Link href={PATH_DASHBOARD_CLEINTE.root} passHref legacyBehavior>
                     <Button
+                        ref={buttonRef}
                         fullWidth
                         color="inherit"
                         variant="outlined"
                         size="medium"
                     >
-                        Regresar al inicio 
+                        Regresar al inicio
                     </Button>
                 </Link>
             </Box>
