766084fcdef25f2991a993f4edf997b108bc41bc
AngelloOZ <angelo-mjz7@hotmail.com>
2023-09-06 16:32:50 -0500

    fix: socket

--- utils/socketClient.ts
+++ utils/socketClient.ts
@@ -3,9 +3,11 @@ import { io, Socket } from "socket.io-client";
 
 let socket: Socket | null = null;
 
+const URL_SERVER = process.env.NEXT_PUBLIC_PORT_SOCKETS || 'http://localhost:9001';
+
 export const getSocket = () => {
     if (!socket) {
-        socket = io("http://localhost:5555"); // Reemplaza con la URL de tu servidor de Socket.io
+        socket = io(URL_SERVER); // Reemplaza con la URL de tu servidor de Socket.io
     }
     return socket;
 };
